fb0070d5e14357556457208a4e839eb0ace095a4
juhong 7586 <juhong0422@gmail.com>
2024-05-31 22:07:28 +0900

    ec2 added

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 from fastapi import FastAPI, File, UploadFile, Request
 from teddy_bear_rasp import TeddyBearRasp
 import shutil
+import httpx 
+import os
+import uuid
 
 app = FastAPI()
 teddy_rasp = TeddyBearRasp()
@@ -13,13 +16,34 @@ async def home():
 	
 @app.post("/upload/")
 async def upload(file: UploadFile = File(...)):
-	with open(f"received_{file.filename}", "wb") as buffer: 
-		shutil.copyfileobj(file.file, buffer)
+	UPLOAD_DIR = './'
+	content = await file.read()
+	filename = f"{str(uuid.uuid4())}.jpg"  # uuid로 유니크한 파일명으로 변경
+	with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
+		fp.write(content)  # 서버 로컬 스토리지에 이미지 저장 (쓰기)
 		
+	return {"filename": filename}
+
+
+# async def download_wav_file(url: str, save_path: str):
+#     async with httpx.AsyncClient() as client:
+#         response = await client.post(url)
+#         response.raise_for_status()  # Raise an exception for HTTP errors
+
+#         with open(save_path, "wb") as f:
+#             f.write(response.content)
+#         print(f"File saved to {save_path}")
+
 
 
 # for test
-@app.post("/listen")
-async def listen(audio: UploadFile = File(...)):
+@app.get("/speak")
+async def speak(audio: UploadFile = File(...)):
 	content = await audio.read()
 	await teddy_rasp.speak(content)
+
+
+
+if __name__ == "__main__":
+	os.system('poetry run python3 listening.py')
+	
